bd0aa9a69c8d2cc8c8c1a15c8adb3a2ee717b063
Evan-2007 <65788884+Evan-2007@users.noreply.github.com>
2024-04-06 13:59:59 -0700

    Fix authentication middleware logic

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,24 @@
+
 import { NextResponse, NextRequest } from "next/server";
 //import getAuth from "@/lib/auth/getAuth";
 
 export async function middleware(req: NextRequest) {
+   const auth = false;
  if (
-    req.nextUrl.pathname.startsWith("/dashboard")
+    req.nextUrl.pathname.startsWith("/dashboard") || req.nextUrl.pathname === "/"
  ) {
-    const res = NextResponse.next();
-    return res;
-//    const auth = getAuth(req);
-//    if (!auth) {
-//      return NextResponse.redirect("/auth/login");
-//    }
+//        const auth = getAuth(req);
+    if (!auth) {
+      const url = req.nextUrl.clone();
+      url.pathname = "/auth/login";
+      return NextResponse.redirect(url);
+    }
+    if (req.nextUrl.pathname === "/") {
+      const url = req.nextUrl.clone();
+      url.pathname = "/dashboard";
+      return NextResponse.redirect(url);
+    }
+   return NextResponse.next();
   } }
 
 
