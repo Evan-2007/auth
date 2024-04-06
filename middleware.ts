66a417cd59bba6a810c2ebccb9b6011040af5d5a
Evan-2007 <65788884+Evan-2007@users.noreply.github.com>
2024-04-06 15:13:56 -0700

    Fix authentication middleware logic and added permissions and api protection

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,19 @@
 
+import { url } from "inspector";
 import { NextResponse, NextRequest } from "next/server";
 //import getAuth from "@/lib/auth/getAuth";
 
 export async function middleware(req: NextRequest) {
-   const auth = false;
+   const auth = {
+      state: false,
+      Permissions: ["user"],
+   }
  if (
-    req.nextUrl.pathname.startsWith("/dashboard") || req.nextUrl.pathname === "/"
+    req.nextUrl.pathname.startsWith("/dashboard") || req.nextUrl.pathname === "/" || req.nextUrl.pathname.startsWith("/auth")
  ) {
 //        const auth = getAuth(req);
-    if (!auth) {
+
+    if (!auth.state && !req.nextUrl.pathname.startsWith("/auth")) {
       const url = req.nextUrl.clone();
       url.pathname = "/auth/login";
       return NextResponse.redirect(url);
@@ -17,11 +22,56 @@ export async function middleware(req: NextRequest) {
       const url = req.nextUrl.clone();
       url.pathname = "/dashboard";
       return NextResponse.redirect(url);
-    }
+   }
+
+      if (auth.state && req.nextUrl.pathname.startsWith("/auth")) {
+         const url = req.nextUrl.clone();
+         url.pathname = "/dashboard";
+         return NextResponse.redirect(url);
+      }
+
+
    return NextResponse.next();
-  } }
+  }
+  
+  
+
+  if (req.nextUrl.pathname.startsWith("/api")) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/auth/login";
+      const path = req.nextUrl.pathname;
+      const route = apiAuthSchema.routes.find((route) => route.path === path);
+      if (!route) {
+         return NextResponse.next();
+      }
+      if (route.Permissions.some((permission) => auth.Permissions.includes(permission))) {
+         return NextResponse.next();
+      }
+      if (route.Permissions.includes("any")) {
+         return NextResponse.next();
+      }
+      return new Response("Unauthorized", { status: 401 });
+  }
+}
 
 
+
+const apiAuthSchema = {
+   routes: [
+       {
+          path: "/api/auth",
+          Permissions: ["any"],
+       },
+       {
+          path: "/api/account",
+            Permissions: ["user"],
+       },
+       {
+         path: "/api/invite",
+         Permissions: ["invites"],
+       },
+    ],
+};
 
 
  
